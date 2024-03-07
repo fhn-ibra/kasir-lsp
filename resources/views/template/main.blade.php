76d770f25a68793dd1186b7a95fb82dc27c98dd2
Ibra <farhanibra21@gmail.com>
2024-03-07 18:35:56 +0700

    fix bug

--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -33,7 +33,7 @@
     <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script>
-    <script src="js/script.js"></script>
+    <script src="js/scripts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
         crossorigin="anonymous"></script>
 
